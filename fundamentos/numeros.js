541591a507eabab2244e570139add15dbccbee39
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2020-06-01 16:29:06 -0300

    Notes about numbers

--- fundamentos/numeros.js
+++ fundamentos/numeros.js
@@ -1,5 +1,5 @@
 const peso1 = 1.0
-const peso2 = Number('2.0')
+const peso2 = Number('2.0') //Number com N maiúsculo é uma função e com n minúsculo é o tipo de dado
 
 console.log(peso1, peso2)
 console.log(Number.isInteger(peso1))
