bf2b811a16f7e9a8af376d17e729f5946b75b759
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2020-06-01 16:49:32 -0300

    Strings

--- fundamentos/numerosCuidadosEspeciais.js
+++ fundamentos/numerosCuidadosEspeciais.js
@@ -1,6 +1,7 @@
 console.log(7 / 0) //Infinity
 console.log('10' / 2) //Reconhece como literal, podendo ser valor de ponto flutuante
+console.log('3' + 2) //Ao colocar o sinal de soma entre string e number o js vai concatenar
 console.log('Show' * 2) //NaN
 console.log(0.1 + 0.7) //0.7999999999999999
 //console.log(10.toString())
-console.log((10.345).toFixed(2))
+console.log((10.345).toFixed(2)) //10.35
